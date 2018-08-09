cffa2f2038c6238e2aefb25bf915a5c25a98e5ac
fdas3213 <szx9404@gmail.com>
2018-08-09 16:18:39 -0700

    finished randomized queue

--- Deques and Randomized Queues/RandomizedQueue.java
+++ Deques and Randomized Queues/RandomizedQueue.java
@@ -1,37 +1,129 @@
 import java.util.Iterator;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
 import java.lang.*;
 import java.util.NoSuchElementException;
 
-public class RandomizedQueue<Item> {
-
-    //create randomized queue using resized array
-    private class ResizingArray{
-        private Item[] array;
-        private int N = 0;
-    }
-
-    private ResizingArray RdQueue;
+public class RandomizedQueue<Item> implements Iterable<Item>{
+    private Item[] array;
+    private int n;
 
     // construct an empty randomized queue
     public RandomizedQueue(){
-        RdQueue = new ResizingArray();
+        array = (Item[]) new Object[2];
+        n = 0;
     }
 
     // is the randomized queue empty?
     public boolean isEmpty(){
-        return RdQueue.N == 0;
+        return n == 0;
     }
 
     // return the number of items on the randomized queue
     public int size()  {
-        return RdQueue.N;
+        return n;
     }
 
+    //implement resizing method: double when full, halve when 1/2
+    private void resizing(int capacity){
+        assert capacity >= n;
+        Item[] temp = (Item[]) new Object[capacity];
+        for (int i = 0; i < n; i ++){
+            temp[i] = array[i];
+        }
+        array = temp;
+    }
 
-    public void enqueue(Item item)           // add the item
-    public Item dequeue()                    // remove and return a random item
-    public Item sample()                     // return a random item (but do not remove it)
-    public Iterator<Item> iterator()         // return an independent iterator over items in random order
-    public static void main(String[] args)   // unit testing (optional)
+    // add the item
+    public void enqueue(Item item){
+        if (item == null){
+            throw new java.lang.IllegalArgumentException("Do not enqueue with null");
+        }
+        if (n == array.length) resizing(array.length * 2);
+        array[n++] = item;
+    }
+
+    // remove and return a random item
+    public Item dequeue(){
+        if (isEmpty()){
+            throw new java.util.NoSuchElementException("Randomized Queue is empty. No more element");
+        }
+
+        int r = StdRandom.uniform(n);
+        Item item = array[r];
+        for (int i = r; i < n-1; i ++){
+            array[i] = array[i+1];
+        }
+        array[n-1] = null;
+        n--;
+
+        if (n > 0 && n == array.length/4) resizing(array.length / 2);
+        return item;
+    }
+
+    @Override
+    public String toString(){
+        String out = "[ ";
+        for (Item item:array){
+            out += item;
+            out += " ";
+        }
+        out += "]";
+        return out;
+    }
+
+    // return a random item (but do not remove it)
+    public Item sample(){
+        if(isEmpty()){
+            throw new java.util.NoSuchElementException("Randomized Queue is empty. No more element");
+        }
+        int rand = StdRandom.uniform(n);
+        return array[rand];
+    }
+
+    // return an independent iterator over items in random order
+    public Iterator<Item> iterator() {return new RQueueIterator();}
+
+    private class RQueueIterator implements Iterator<Item>{
+        public boolean hasNext(){
+            return n != 0;
+        }
+
+        public void remove() { throw new java.lang.UnsupportedOperationException("remove is not supported");}
+
+        public Item next(){
+            if (!hasNext()){
+                throw new java.util.NoSuchElementException("Randomized Queue is empty. No more element");
+            }
+            return dequeue();
+        }
+    }
+
+    // unit testing (optional)
+    public static void main(String[] args){
+        RandomizedQueue<Integer> rq = new RandomizedQueue<Integer>();
+
+        StdOut.println("is empty: " + rq.isEmpty());
+        StdOut.println("size of rq: " + rq.size());
+
+        rq.enqueue(10);
+        rq.enqueue(3);
+
+        StdOut.println("is empty: " + rq.isEmpty());
+        StdOut.println("size of rq: " + rq.size());
+        rq.enqueue(7);
+        rq.enqueue(5);
+        StdOut.println(rq.toString());
+
+        int s = rq.sample();
+        StdOut.println("sample: " + s);
+
+        int out1 = rq.dequeue();
+        StdOut.println("1st out element: " + out1);
+        int out2 = rq.dequeue();
+        StdOut.println("2nd out element: " + out2);
+        StdOut.println(rq.toString());
+    }
+
 }
