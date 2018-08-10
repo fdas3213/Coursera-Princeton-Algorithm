c107bfe0ce925a6ed1944a588568b638ed9423d9
fdas3213 <szx9404@gmail.com>
2018-08-09 17:01:03 -0700

    added permutation

---
+++ Deques and Randomized Queues/Permutation.java
@@ -0,0 +1,18 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Permutation{
+    public static void main(String[] args){
+        StdOut.println(StdIn.isEmpty());
+
+
+        /*
+        RandomizedQueue<String> rq = new RandomizedQueue<String> ();
+
+        int k = Integer.parseInt(args[0]);
+        for (int i = 0; i < k; i ++){
+            rq.enqueue(args[i+1]);
+        }
+        */
+    }
+}
